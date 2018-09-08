2d90bdbd9a949847aba9b29060411387b683a9b7
Gopal <Gopal@DESKTOP-ADQB615>
2018-09-08 03:07:00 -0400

    Created a New Rest Service

--- Demo/src/main/java/com/example/demo/controller/DemoController.java
+++ Demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+	
+	@GetMapping(path = "/getName")
+	public String getName() {
+		return "Demo";
+	}
 
 }
