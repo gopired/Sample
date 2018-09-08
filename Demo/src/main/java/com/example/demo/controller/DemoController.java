28d8e81ff6e0924567f7116ae710aedff2c9909d
Gopal <Gopal@DESKTOP-ADQB615>
2018-09-08 16:02:22 -0400

    Created Methods for Update, Delete, and Insert

--- Demo/src/main/java/com/example/demo/controller/DemoController.java
+++ Demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,19 @@ public class DemoController {
 	public String getName() {
 		return "Demo";
 	}
+	@PutMapping(path = "/updateName")
+	public String putName(){
+		return "DemoPut";
+	}
+	@PostMapping(path="/insertName")
+	public String updateName(){
+		return"Sucessfully Inserted ";
+	}
+	
+	@DeleteMapping(path="/deleteName")
+	public String deleteName(){
+		return"Sucessfully Deleted";
+	}
+	
 
 }
